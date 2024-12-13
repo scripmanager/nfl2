0283d5ef20fc23b3cc5b9011939dac71b5c0edf4
benson304 <benson304@gmail.com>
2024-12-13 15:37:16 -0600

    livewire

--- config/livewire.php
+++ config/livewire.php
@@ -12,8 +12,6 @@ return [
     | finds components. It's also referenced by the file creation commands.
     |
     */
-
-    'asset_url' => env('APP_URL') . '/livewire/livewire.js',
     'class_namespace' => [
         'App\\Http\\Livewire',
         'App\\Http\\Livewire\\BulkStatsUpdate' => 'bulk-stats-update',
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,4 @@
-import './bootstrap';
 import * as Livewire from 'livewire';
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
-window.Livewire = Livewire;
