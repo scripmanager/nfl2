efeec87a40092636934cd36402a9468a4c4d0b68
benson304 <benson304@gmail.com>
2024-12-13 15:28:29 -0600

    update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -13,6 +13,7 @@ return [
     |
     */
 
+    'asset_url' => env('APP_URL') . '/livewire',
     'class_namespace' => [
         'App\\Http\\Livewire',
         'App\\Http\\Livewire\\BulkStatsUpdate' => 'bulk-stats-update',
