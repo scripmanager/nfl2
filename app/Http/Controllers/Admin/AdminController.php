83ff253200364d0b3838e494b00fe5d33a00f38d
benson304 <benson304@gmail.com>
2024-12-13 16:16:53 -0600

    update admincontroller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -12,7 +12,7 @@ use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-
+use App\Http\Controllers\Controller;
 class AdminController extends Controller
 {
    public function index()
