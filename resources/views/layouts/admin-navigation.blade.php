5aff24ed894c1aedfe3e77179d1926704b89de43
benson304 <benson304@gmail.com>
2024-12-15 10:34:09 -0600

    Comment out manage account

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -42,11 +42,11 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <!-- Account Management -->
+                        <!-- Account Management 
                         <div class="block px-4 py-2 text-sm text-nfl-primary">
                             {{ __('Manage Account') }}
                         </div>
-
+                        -->
                         <!-- Logout -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
