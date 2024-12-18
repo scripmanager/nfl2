991a19042baafe4fdf81b73048f1095055a4cf04
scripmanager <48145695+scripmanager@users.noreply.github.com>
2024-12-18 14:23:00 -0600

    migration (#10)
    * Changes to entry roster page.
    
    Max changes now determined by Entry->getChangesRemaining(). It takes 10- entry_players count this if 2 changes they will have 10 records resulting in 0 changes remaining.
    
    //roster.blade.php
    -Grays players that are out
    -Disables change button if max changes reached
    -Added revert button for changed players if they havent started their next game yet. **Need to test more
    -shows correct points, changes remaining, players active in summary
    
    NOTE: This may break some things elsewhere I only worked on the roster page.  I didn't change any existing model relations so I dont expect any issues. There are some complex queries but no updates so hipefully we dont run into more sqlite problems like we had with removed_at update.
    
    * Changes to entry roster page.
    
    Max changes now determined by Entry->getChangesRemaining(). It takes 10- entry_players count this if 2 changes they will have 10 records resulting in 0 changes remaining.
    
    //roster.blade.php
    -Grays players that are out
    -Disables change button if max changes reached
    -Added revert button for changed players if they havent started their next game yet. **Need to test more
    -shows correct points, changes remaining, players active in summary
    
    NOTE: This may break some things elsewhere I only worked on the roster page.  I didn't change any existing model relations so I dont expect any issues. There are some complex queries but no updates so hipefully we dont run into more sqlite problems like we had with removed_at update.
    
    * EntryController revertChangeData minor date query fix
    
    * Reworked roster cancel change function
    
    * More roster page changes.
    
    NEED TO run migration to add deleted_at to transactions table.  Will use this to store log of cancelled changes
    
    * More roster page changes.
    Started Flex SWAP
    
    * migration

---
+++ database/migrations/2024_12_18_141844_add_winning_team_id_games.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('games', function (Blueprint $table) {
+            $table->integer('winning_team_id')->after('away_score')->default(0);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
